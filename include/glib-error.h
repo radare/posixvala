a325d070c9a2db1d3be1d271b5cc2e49efedd650
xdave <davehome@redthumb.info.tm>
2013-09-27 11:57:12 -0600

    improve GQuark handling and GError creation.

--- include/glib-error.h
+++ include/glib-error.h
@@ -3,8 +3,10 @@
 #ifndef _GLIB_ERROR_H_
 #define _GLIB_ERROR_H_
 
-static inline const char *g_quark_to_string(GQuark q) { return "Error"; }
-#define g_error_new(x,y,z) (GError*)strdup (z)
+static inline const gchar *g_quark_to_string(GQuark q) {
+	return (const gchar *)q;
+}
+#define g_error_new(x,y,z) g_error_new_literal(x, y, z)
 #define g_error_free(x) free(x->message),free(x)
 #define g_clear_error free
 #define g_propagate_error(x,y) *x=y
@@ -14,7 +16,7 @@ struct _GError {
 	gint         code;
 	gchar       *message;
 };
-static inline GError *g_error_new_literal(int x, int y, const char *msg) {
+static inline GError *g_error_new_literal(GQuark x, gint y, const gchar *msg) {
 	GError *err = g_new0 (GError, 1);
 	err->domain = x;
 	err->code = y;
